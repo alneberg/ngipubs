1b9afd149902ef2b28ff2164cbb63a7eb119d8a2
Johannes Alneberg <alneberg@kth.se>
2018-12-11 15:51:46 +0100

    Comment functionality independent of status updates

--- js/app.js
+++ js/app.js
@@ -250,15 +250,13 @@ $(document).ready(function() {
 	$('.verify_button').on('click', function() {
 		var button = this;
 		var id = $(this).attr('id').split('-')[1];
-		var user_comment = $('#comment-' + id).val();
 		$.ajax({
 			url: '_publication_verify.php',
-			data: { publication_id: id, type: 'verify', comment: user_comment},
+			data: { publication_id: id, type: 'verify'},
 			dataType: 'json',
 			success: function(json) {
 				$('#status_label-' + id).text('Verified').addClass('success');
 				$('#publ-' + id).addClass('success');
-				$('#comment-' + id).val("");
 			}
 		});
 	});
@@ -266,15 +264,13 @@ $(document).ready(function() {
 	$('.discard_button').on('click', function() {
 		var button = this;
 		var id = $(this).attr('id').split('-')[1];
-		var user_comment = $('#comment-' + id).val();
 		$.ajax({
 			url: '_publication_verify.php',
-			data: { publication_id: id, type: 'discard', comment: user_comment},
+			data: { publication_id: id, type: 'discard'},
 			dataType: 'json',
 			success: function(json) {
 				$('#status_label-' + id).text('Discarded').addClass('alert');
 				$('#publ-' + id).addClass('alert');
-				$('#comment-' + id).val("");
 			}
 		});
 	});
@@ -282,14 +278,26 @@ $(document).ready(function() {
 	$('.maybe_button').on('click', function() {
 		var button = this;
 		var id = $(this).attr('id').split('-')[1];
-		var user_comment = $('#comment-' + id).val();
 		$.ajax({
 			url: '_publication_verify.php',
-			data: { publication_id: id, type: 'maybe', comment: user_comment},
+			data: { publication_id: id, type: 'maybe'},
 			dataType: 'json',
 			success: function(json) {
 				$('#status_label-' + id).text('Maybe').addClass('warning');
 				$('#publ-' + id).addClass('warning');
+			}
+		});
+	});
+
+	$('.comment_button').on('click', function() {
+		var button = this;
+		var id = $(this).attr('id').split('-')[1];
+		var user_comment = $('#comment-' + id).val();
+		$.ajax({
+			url: '_publication_comment.php',
+			data: { publication_id: id, comment: user_comment},
+			dataType: 'json',
+			success: function(json) {
 				$('#comment-' + id).val("");
 			}
 		});
