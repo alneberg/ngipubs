1fad56ae2ef57a4e77aa1dd222a5c39ebab5592d
Johannes Alneberg <alneberg@kth.se>
2019-01-21 11:46:51 +0100

    Replacing the normalize function for checking PI:s as well

--- lib/class.NGIresearchers.php
+++ lib/class.NGIresearchers.php
@@ -686,14 +686,26 @@ class NGIresearchers {
 		}
 	}
 
-	private function normalize($string) {
-	    $a = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿŔŕ';
-	    $b = 'aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr';
-	    $string = strtr($string, $a, $b);
-	    $string = strtolower($string);
-	    $string = preg_replace('/\s+/', '', $string);
-	    $string = str_replace('-', '', $string);
-	    return $string;
+	private function normalize ($string) {
+		/* Stolen from php.net. Not fool-proof since there is no direct
+		translation between udf-8 and ascii */
+		$table = array(
+			'Š'=>'S', 'š'=>'s', 'Đ'=>'Dj', 'đ'=>'dj', 'Ž'=>'Z', 'ž'=>'z', 'Č'=>'C', 'č'=>'c', 'Ć'=>'C', 'ć'=>'c',
+			'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+			'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+			'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
+			'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+			'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
+			'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
+			'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r', 'ü'=>'u', 'ė'=>'e', 'ą'=>'a', 'ū'=>'u', 'Ł'=>'L', 'ś'=>'s', 'ł'=>'l',
+			'ş'=>'s', 'ń'=>'n', 'ğ'=>'g', 'ę'=>'e', '’'=>"'", 'ő'=>'o', 'ı'=>'l',
+		);
+
+		$string = strtr($string, $table);
+		$string = strtolower($string);
+		$string = preg_replace('/\s+/', '', $string);
+		$string = str_replace('-', '', $string);
+		return $string;
 	}
 
 	private function addLog($message,$action,$json=FALSE) {
